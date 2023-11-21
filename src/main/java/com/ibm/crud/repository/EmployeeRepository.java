eaae7ef3079db4809978b0dd63d483454c4300ea
Satish Bharatiya <bharadwajss@gmail.com>
2023-11-21 20:57:38 +0530

    This is my first crud operation

--- src/main/java/com/ibm/crud/repository/EmployeeRepository.java
+++ src/main/java/com/ibm/crud/repository/EmployeeRepository.java
@@ -4,7 +4,7 @@ import com.ibm.crud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Repository //this is optional annotation
 public interface EmployeeRepository  extends JpaRepository<Employee,Integer> {
 
 }
